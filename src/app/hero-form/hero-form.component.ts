1e1b819262bed18a8b2bf16f78b411c37124a27b
Isaac Martinez <pcalchi@aim.com>
2017-03-07 23:30:35 -0500

    Lint Errors

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -3,9 +3,10 @@ import { Hero } from './hero';
 @Component({
   selector: 'app-hero-form',
   templateUrl: './hero-form.component.html',
-  styleUrls: ['./hero-form.component.scss']
+  styleUrls: [ './hero-form.component.scss']
 })
-export class HeroFormComponent implements OnInit {
+export class HeroFormComponent implements OnInit 
+{
 
   powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
   model = new Hero(0, '', this.powers[0], null);
@@ -16,5 +17,4 @@ export class HeroFormComponent implements OnInit {
   onSubmit() {
     this.submitted = true;
     console.log(JSON.stringify(this.model));
-  }
-}
+  }}
